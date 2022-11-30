046da8f3baeeeeda9b22634fd66a904c9cda91e2
luqmanito <luqmangrahito@gmail.com>
2022-11-30 16:02:04 +0700

    add-express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+require ("dotenv").config()
+
+const express = require("express")
+
+const PORT = 8080
+server.listen(PORT, ()=>{
+    console.log('server running at' + PORT);
+})
